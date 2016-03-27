4b51f911187546bd5152d6dd5faec802557164e1
Jon Hammond <jonh1016@gmail.com>
2016-03-27 15:19:59 -0600

    Finished Section 5

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@
 var app = angular.module('myApp', []);
 
 app.controller('myController', ['$scope', function($scope) {
+  $scope.sorts = '';
   $scope.greeting = "Exercises!";
   $scope.avalanchePlayers = [
     {fname: "Joe", lname: "Sakic"},
@@ -22,4 +23,27 @@ app.controller('myController', ['$scope', function($scope) {
     {fname: "Andres", lname: "Galaragga"},
     {fname: "Larry", lname: "Walker"},
     ];
+  $scope.cameras = [
+  {
+    title: 'Nikon D3100 DSLR',
+    image: 'http://ecx.images-amazon.com/images/I/713u2gDQqML._SX522_.jpg',
+    rating: 3.4,
+    price: 369.99,
+    onSale: true
+  },
+  {
+    title: 'Canon EOS 70D',
+    image: 'http://ecx.images-amazon.com/images/I/81U00AkAUWL._SX522_.jpg',
+    rating: 2.0,
+    price: 1099.0,
+    onSale: false
+  },
+  {
+    title: 'Nikon D810A',
+    image:'http://ecx.images-amazon.com/images/I/91wtXIfLl2L._SX522_.jpg',
+    rating: 4.2,
+    price: 3796.95,
+    onSale: true
+  }
+];
 }]);
